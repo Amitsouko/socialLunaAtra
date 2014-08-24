fd6fddf18a0c6cc20f72951411efc8897f683c64
Amitsouko <benoit.marcanthony@gmail.com>
2014-08-24 15:44:35 +0200

    symfony

---
+++ src/LunaAtra/CoreBundle/CoreBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace LunaAtra\CoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CoreBundle extends Bundle
+{
+}
---
+++ src/LunaAtra/ProfileBundle/ProfileBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace LunaAtra\ProfileBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ProfileBundle extends Bundle
+{
+}
