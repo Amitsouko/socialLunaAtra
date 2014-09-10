d51f965691ae82672b99366125330f37c2780c08
Amitsouko <benoit.marcanthony@gmail.com>
2014-09-10 18:26:21 +0200

    push characterController

---
+++ src/LunaAtra/ProfileBundle/Controller/CharacterController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace LunaAtra\ProfileBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * @Route("/characters")
+ */
+class CharacterController extends Controller
+{
+
+
+    /**
+     * @Route("/{id}", name="user-character")
+     * @Template("ProfileBundle:Default:character.html.twig")
+     */
+    public function CharacterAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $character = $em->getRepository('ProfileBundle:Charact')->findOneById($id);
+        return array('user' =>$character->getUser(), "character" => $character);
+    }
+
+}
