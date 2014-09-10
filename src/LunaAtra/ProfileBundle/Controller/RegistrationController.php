2730dd60c565b63aa38211e703867c1ee6344a4e
Amitsouko <benoit.marcanthony@gmail.com>
2014-09-10 18:22:33 +0200

    change redirect after subscribe

--- src/LunaAtra/ProfileBundle/Controller/RegistrationController.php
+++ src/LunaAtra/ProfileBundle/Controller/RegistrationController.php
@@ -116,7 +116,7 @@ class RegistrationController extends ContainerAware
                 $userManager->updateUser($user);
 
                 if (null === $response = $event->getResponse()) {
-                    $url = $this->container->get('router')->generate('fos_user_registration_confirmed');
+                    $url = $this->container->get('router')->generate('fos_user_profile_show');
                     $response = new RedirectResponse($url);
                 }
 
